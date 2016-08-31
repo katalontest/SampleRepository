f786a40c2da694bac6fc5dd4c7c2b07cbd91df1b
phucla <phucla@F9N0CJ037028389.kms.com.vn>
2016-08-31 08:29:18 +0700

    test commit

---
+++ Object Repository/New Test Object.rs
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <elementGuidId>1764d4ac-b992-4ec9-b439-8cab7d816427</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c8e91381-2138-4515-9d39-657d9d02bf53</testSuiteGuid>
+</TestSuiteEntity>
